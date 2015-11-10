60060991c3c60e8d60805efe581d75ee24e89b9a
peidong <peidong@outlook.com>
2015-11-09 17:42:20 -0800

    nest server

---
+++ Server/api/nest_sensor/get/index.php
@@ -0,0 +1,49 @@
+<?php
+    header("Content-Type:application/json");
+
+    // start the session
+    session_start();
+
+    // close the session
+    session_write_close();
+
+    $data_array = get_nest_sensor_data();
+    $humidity = $data_array['thermostats']['tM0i4N8lworgCTw1RFF1B5NeGNgy1FqW']['humidity'];
+    $current_temperature = $data_array['thermostats']['tM0i4N8lworgCTw1RFF1B5NeGNgy1FqW']['ambient_temperature_f'];
+    $co_alarm_state = $data_array['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['co_alarm_state'];
+    $smoke_alarm_state = $data_array['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['smoke_alarm_state'];
+
+    $response['humidity'] = $humidity;
+    $response['current_temperature'] = $current_temperature;
+    $response['co_alarm_state'] = $co_alarm_state;
+    $response['smoke_alarm_state'] = $smoke_alarm_state;
+
+    deliver_response(200, "The nest sensors' data has been got", $response);
+
+    function get_nest_sensor_data(){
+        $url = "https://developer-api.nest.com/devices.json?auth=c.T1yBuMT940gRUCMCrdNgyw9f3D8jPDcDrq2JunxVxbZ9qsJk9V6Ji8bcgTRocZvh3wvDaG8TDbQ0eR5yRUuXLMyqlNheArebuhNLv0tknBjzcvVVdPmHqFFvmE5z0GrBOJV4hkCvMksLHwCA";
+        $headers = array("Content-type: application/json;charset='utf-8'", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache",);
+        $ch = curl_init();
+        $timeout = 30;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data_array = json_decode($data, true);
+        return $data_array;
+    }
+
+
+    function deliver_response($status,$status_message,$data){
+        header("HTTP/1.1 $status $status_message");
+        $response['status']=$status;
+        $response['status_message']=$status_message;
+        $response['data']=$data;
+
+        $json_response=json_encode($response);
+        echo $json_response;
+    }
+?>
--- nest-api/nest.py
+++ nest-api/nest.py
@@ -7,25 +7,29 @@ import threading
 #http://mysql-python.sourceforge.net/MySQLdb.html
 import MySQLdb
 
-results_raw = requests.get("https://developer-api.nest.com/devices.json?auth=c.Q2TYtjcqbU1Ypr9J5I2dQEsmXHaHFbiFIMrqh67jrMlsbiNEIdvMIEHMAAazWhh49YuCEzk1VqE4gAsoasOjgpJLl8xo2KhadllPeJZNlNttb0t61SXgNWAvZOQLupxXDjpWz3MxEj8INiPU")
+results_raw = requests.get("https://developer-api.nest.com/devices.json?auth=c.T1yBuMT940gRUCMCrdNgyw9f3D8jPDcDrq2JunxVxbZ9qsJk9V6Ji8bcgTRocZvh3wvDaG8TDbQ0eR5yRUuXLMyqlNheArebuhNLv0tknBjzcvVVdPmHqFFvmE5z0GrBOJV4hkCvMksLHwCA")
 results_json = results_raw.text
 results = yaml.safe_load(results_json)
 
 humidity = int(results['thermostats']['tM0i4N8lworgCTw1RFF1B5NeGNgy1FqW']['humidity'])
 current_temperature = int(results['thermostats']['tM0i4N8lworgCTw1RFF1B5NeGNgy1FqW']['ambient_temperature_f'])
+co_alarm_state = results['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['co_alarm_state']
+smoke_alarm_state = results['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['smoke_alarm_state']
 
-print ("humidity = " + str(humidity))
 print ("current_temperature = " + str(current_temperature))
+print ("humidity = " + str(humidity))
 
-db=MySQLdb.connect(host="localhost",user="webmaster",db="nest")
-db.autocommit(True)
-cur = db.cursor()
+print ("co_alarm_state = " + co_alarm_state)
+print ("smoke_alarm_state= " + smoke_alarm_state)
+# db=MySQLdb.connect(host="localhost",user="webmaster",db="nest")
+# db.autocommit(True)
+# cur = db.cursor()
 
-cur.execute("UPDATE thermostat SET current_temperature = %s, humidity = %s, update_time = now() WHERE id = 1", (str(current_temperature), str(humidity)))
+# cur.execute("UPDATE thermostat SET current_temperature = %s, humidity = %s, update_time = now() WHERE id = 1", (str(current_temperature), str(humidity)))
 
-cur.execute("SELECT * FROM thermostat")
-for row in cur.fetchall():
-    print row[0]
-    print row[1]
-    print row[2]
-    print row[3]
+# cur.execute("SELECT * FROM thermostat")
+# for row in cur.fetchall():
+    # print row[0]
+    # print row[1]
+    # print row[2]
+    # print row[3]
