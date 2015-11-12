b83613fb250a0bcc1ca97310e1d65df23cfcfb1f
peidong <peidong@outlook.com>
2015-11-12 11:53:40 -0800

    nest server

--- nest-api/et.py
+++ nest-api/et.py
@@ -30,15 +30,15 @@ import math
 def asceDaily(year, month, day, fTMinC, fTMaxC, fU2z, fU2m, fLat, fElevation, fRs, fEa, fRHMin, fRHMax, fPressure, fKrs, fTDewpointC):
         #//////////day of year////////////
         fJ = day - 32 + math.floor(275 * month / 9.0) + 2 * math.floor(3.0 / (month + 1)) + math.floor(month / 100.0 - (year % 4) / 4.0 + 0.975);#Eq.25
-        #print("->Day of year:", fJ);
+        print("->Day of year:", fJ);
         #/////////temperatures////////////
         fTMeanC = (fTMinC + fTMaxC) / 2;
         fTMinK = 273.16 + fTMinC;
         fTMaxK = 273.16 + fTMaxC;
-        #print("->TMeanC:", fTMeanC, "TMinK:",fTMinK, "TMaxK:",fTMaxK);
+        print("->TMeanC:", fTMeanC, "TMinK:",fTMinK, "TMaxK:",fTMaxK);
         #/////////delta///////////////////
         fDelta = 2503.0 * math.exp(17.27 * fTMeanC / (fTMeanC + 237.3)) / math.pow(fTMeanC + 237.3, 2);#Eq.5
-        #print("->Delta:", fDelta);
+        print("->Delta:", fDelta);
         #//////// es /////////////////////
         fETMax = 0.6108 * math.exp(17.27 * fTMaxC / (fTMaxC + 237.3));#Eq.7
         fETMin = 0.6108 * math.exp(17.27 * fTMinC / (fTMinC + 237.3));#Eq.7
@@ -54,32 +54,32 @@ def asceDaily(year, month, day, fTMinC, fTMaxC, fU2z, fU2m, fLat, fElevation, fR
                                 fEa = 0.6108 * math.exp(17.27 * fTDewpointC / (fTDewpointC + 237.3));#Eq.8 Ea from TDew
                         else:
                                 fEa = 0.6108 * math.exp(17.27 * fTMinC / (fTMinC + 237.3));#Eq.8 Ea from TDew aproximated with TMin
-        #print("->Ea:", fEa, "Es:", fEs);
+        print("->Ea:", fEa, "Es:", fEs);
         #////////u2//////////////////////
         fU2 = 0.0;#default value 0 m/s (no wind)
         if fU2m == None:
                 fU2m = 2;#by default wind is measured at 2m height
         if fU2z != None:
                 fU2 = fU2z * 4.87 / math.log(67.8 * fU2m - 5.42);#Eq.33
-        #print("->Wind at 2m:", fU2);
+        print("->Wind at 2m:", fU2);
         #////////dr//////////////////////
         fDr = 1.0 + 0.033 * math.cos(2 * math.pi / 365 * fJ);#Eq.23
-        #print("->Dr:", fDr);
+        print("->Dr:", fDr);
         #////////declin//////////////////////
         fDeclin = 0.409 * math.sin(2 * math.pi / 365 * fJ - 1.39);#Eq.24
-        #print("->Declin:", fDeclin);
+        print("->Declin:", fDeclin);
         #///////omegas///////////////////////
         fLatRadian = math.pi / 180.0 * fLat;
-        #print "->Lat Radian:", fLatRadian;
+        print "->Lat Radian:", fLatRadian;
         fOmegaPreprocess = -math.tan(fLatRadian) * math.tan(fDeclin);
-        #print "->OmegaPreprocess:", fOmegaPreprocess;
+        print "->OmegaPreprocess:", fOmegaPreprocess;
         if fOmegaPreprocess > 1.0:
                 fOmegaPreprocess = 1.0;
         else:
                 if fOmegaPreprocess < -1.0:
                         fOmegaPreprocess = -1.0;
         fOmegaS = math.acos(fOmegaPreprocess);#Eq.27
-        #print("->OmegaS:", fOmegaS);
+        print("->OmegaS:", fOmegaS);
         #////////radiation stuff ////////////
         fRa = 24.0 / math.pi * 4.92 * fDr * (fOmegaS * math.sin(fLatRadian) * math.sin(fDeclin) + math.cos(fLatRadian) * math.cos(fDeclin) * math.sin(fOmegaS) );#Eq.21
         fRSo = 0.0;
@@ -118,15 +118,15 @@ def asceDaily(year, month, day, fTMinC, fTMaxC, fU2z, fU2m, fLat, fElevation, fR
         fRns = 0.77 * fRs;#Eq.16
         fRn = fRns - fRnl;#Eq.15
 
-        #print("->Ra:", fRa, "Rso:", fRSo, "Rnl:", fRnl);
-        #print("->Rns:", fRns, "Rn:", fRn);
+        print("->Ra:", fRa, "Rso:", fRSo, "Rnl:", fRnl);
+        print("->Rns:", fRns, "Rn:", fRn);
 
         #///////////Pressure//////////////////
         if fPressure == None:
                 fPressure = 101.3 * pow((293 - 0.0065 * fElevation) / 293, 5.25);#Eq.3
         PSYCON = 0.000665;
         fPsyCon = PSYCON * fPressure;#Eq.4
-        #print("->Pressure:", fPressure);
+        print("->Pressure:", fPressure);
 
         #///////////ET//////////////////
         fCn = 900.0;
@@ -142,7 +142,7 @@ def asceDaily(year, month, day, fTMinC, fTMaxC, fU2z, fU2m, fLat, fElevation, fR
         if fETos < 0.0: # don't allow negative ET0
                 fETos = 0.0
 
-        #print("->ETos:", fETos, "ETrs:", fETrs);
+        print("->ETos:", fETos, "ETrs:", fETrs);
 
         return fETos;
 
@@ -151,8 +151,8 @@ def asceDaily(year, month, day, fTMinC, fTMaxC, fU2z, fU2m, fLat, fElevation, fR
 if __name__ == '__main__':
         #               year  ,month,day  ,minT ,maxT ,wind ,windalt,lat deg,elev(m),solar rad  , Ea(hum), RhMin ,RhMax  ,pressure   ,Krs  , TDew
         et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , 16.502    , 1.4    , None  , None  , None      , 0.17, None);print("ET0=",et0);#every value is valid
-        et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , None      , 1.4    , None  , None  , None      , 0.17, None);print("ET0=",et0);#solar radiation from temperatures
-        et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , 16.502    , None   , 36.0  , 91.0  , None      , 0.17, None);print("ET0=",et0);#humidity from relative hum
-        et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , 16.502    , None   , None  , None  , None      , 0.17, 11.7);print("ET0=",et0);#humidity from TDew
-        et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, None, None  , 36.82 , 98.5  , None      , None   , None  , None  , None      , None, None);print("ET0=",et0);#everything from temp
+        # et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , None      , 1.4    , None  , None  , None      , 0.17, None);print("ET0=",et0);#solar radiation from temperatures
+        # et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , 16.502    , None   , 36.0  , 91.0  , None      , 0.17, None);print("ET0=",et0);#humidity from relative hum
+        # et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, 1.3 , 2     , 36.82 , 98.5  , 16.502    , None   , None  , None  , None      , 0.17, 11.7);print("ET0=",et0);#humidity from TDew
+        # et0 = asceDaily(2012.0, 10.0, 15.0, 10.7, 27.3, None, None  , 36.82 , 98.5  , None      , None   , None  , None  , None      , None, None);print("ET0=",et0);#everything from temp
 
