481b3ebb82b014d3fa1bfe2975956dd5c9bc1817
peidong <peidong@outlook.com>
2015-11-13 15:25:34 -0800

    server

--- Server/api/nest_sensor/get/index.php
+++ Server/api/nest_sensor/get/index.php
@@ -3,9 +3,11 @@
 
     // start the session
     session_start();
+    $sn_number = $_GET['sn_number'];
 
     // close the session
     session_write_close();
+
 
     $data_array = get_nest_sensor_data();
     $humidity = $data_array['thermostats']['tM0i4N8lworgCTw1RFF1B5NeGNgy1FqW']['humidity'];
@@ -13,30 +15,50 @@
     $co_alarm_state = $data_array['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['co_alarm_state'];
     $smoke_alarm_state = $data_array['smoke_co_alarms']['rI1VFhpj1DFSXGHO6QnisJNeGNgy1FqW']['smoke_alarm_state'];
 
-    $response['humidity'] = $humidity;
-    $response['current_temperature'] = $current_temperature;
+    //$response['humidity'] = $humidity;
+    //$response['current_temperature'] = $current_temperature;
     $response['co_alarm_state'] = $co_alarm_state;
     $response['smoke_alarm_state'] = $smoke_alarm_state;
 
+    $water_time_min = process_sensor_data($sn_number, $smoke_alarm_state, $co_alarm_state);
+    $response['water_time_min'] = $water_time_min;
+
     deliver_response(200, "The nest sensors' data has been got", $response);
 
-    function process_sensor_data(){
+    function process_sensor_data($sn_number, $smoke_alarm_state, $co_alarm_state){
+        $water_zone_number = 1;
+        $devid = get_devid($sn_number);
+        $status = get_device_status($sn_number);
+        $n_status = (int)$status;
         if ($smoke_alarm_state == "ok" && $co_alarm_state == "ok"){
             //stop watering
-            send_water_command(0);
+            if($n_status == 0){
+                return -1;
+            }else{
+                send_RainClose_command($devid);
+                return 0;
+            }
         }else if ($smoke_alarm_state == "emergency" || $co_alarm_state == "emergency"){
-            //water 1min
-            send_water_command(1);
-        }else{
             //water 5min
-            send_water_command(5);
+            if($n_status == $water_zone_number){
+                return -3;
+            }else{
+                send_RainOnce_command($devid, 5);
+                return 5;
+            }
+        }else{
+            //water 1min
+            if($n_status == $water_zone_number){
+                return -2;
+            }else{
+                send_RainOnce_command($devid, 1);
+                return 1;
+            }
         }
     }
-            
-    function send_water_command($water_time_min){
-        $device_id = "";
-        $water_zone_number = 1;
-        $url = "";
+
+    function get_device_status($sn_number){
+        $url = "http://www.rainconn.com/api/rest/client/devstatus?sn=$sn_number";
         $headers = array("Content-type: application/json;charset='utf-8'", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache",);
         $ch = curl_init();
         $timeout = 30;
@@ -45,10 +67,60 @@
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "devid=$device_id&zone=$water_zone_number&watertime=$water_time_min");
         $server_output = curl_exec($ch);
         curl_close($ch);
+        $server_output_array = json_decode($server_output, true);
+        $watering_zone_number = $server_output_array['data']['now'];
+        return $watering_zone_number;
+    }
+
+    function send_RainOnce_command($devid, $water_time_min){
+        $water_zone_number = 1;
+        $url = "http://www.rainconn.com/api/rest/client/sendcommand?&devid=$devid&command=RainOnce&params=param:3;type:1;whichvalve:$water_zone_number;howlong:$water_time_min&srcip=www.rainconn.com&srctype=2";
+        $headers = array("Content-type: application/json;charset='utf-8'", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache",);
+        $ch = curl_init();
+        $timeout = 30;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        //curl_setopt($ch, CURLOPT_POST, 1);
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, "devid=$device_id&zone=$water_zone_number&watertime=$water_time_min");
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+    }
+
+    function send_RainClose_command($devid){
+        $water_zone_number = 1;
+        $url = "http://www.rainconn.com/api/rest/client/sendcommand?&devid=$devid&command=RainClose&params=param:2;type:1;whichvalve:$water_zone_number";
+        $headers = array("Content-type: application/json;charset='utf-8'", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache",);
+        $ch = curl_init();
+        $timeout = 30;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+    }
+
+    function get_devid($sn_number){
+        $url = "http://www.rainconn.com/api/rest/client/getdevidbysno?sno=$sn_number";
+        $headers = array("Content-type: application/json;charset='utf-8'", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache",);
+        $ch = curl_init();
+        $timeout = 30;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data_array = json_decode($data, true);
+        $devid = $data_array['data']['devid'];
+        return $devid;
     }
 
     function get_nest_sensor_data(){
